11be64d2eeabfa4a9da07f73fdd4cb5545a9f018
mustafa <mustafadervisoglu16@gmail.com>
2022-10-26 17:05:22 +0300

    multicall

--- multicall-js.js
+++ multicall-js.js
@@ -1,20 +1,20 @@
 const {MultiCall} = require('@indexed-finance/multicall')
-const {abi} = require("./AggegatorV3Interface.json");
-const priceFeedAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
+const {abi} = require("./aggregator.json");
+const contractAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
 const ethers = require('ethers')
 const fs = require('fs');
 
 async function main() {
     let provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/YOUR_API_KEY')
-    const multi = new MultiCall(provider)
+    const multiCall = new MultiCall(provider)
     const inputs = []
     let numberOfRounds = 10
-    let priceFeed = new ethers.Contract(priceFeedAddress, abi, provider)
-    let latestRound = (await priceFeed.latestRoundData())[0]
+    let contract = new ethers.Contract(contractAddress, abi, provider)
+    let latestRound = (await contract.latestRoundData())[0]
     for (let round = latestRound.sub(numberOfRounds); round.lt(latestRound); round = round.add(1)) {
-        inputs.push({target: priceFeedAddress, function: 'getRoundData', args: [round.toString()]})
+        inputs.push({target: contractAddress, function: 'getRoundData', args: [round.toString()]})
     }
-    const roundData = await multi.multiCall(abi, inputs)
+    const roundData = await multiCall.multiCall(abi, inputs)
     fs.writeFileSync('test.json', JSON.stringify(roundData))
     console.log(roundData)
     return roundData
